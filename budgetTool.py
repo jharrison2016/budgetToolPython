982adcb6292d4eeeb6acaaa6773a6dd513c62c7c
jharrison2016 <harrisonjake4@gmail.com>
2019-05-24 23:03:06 -0400

    added function skeletons

---
+++ budgetTool.py
@@ -0,0 +1,23 @@
+main()
+
+def main():
+	print("Choose an option: \n")
+	print("Spend	Add		View")
+
+def spend(accountValue, spendValue):
+	
+
+def add(accountValue, addValue):
+
+
+def viewAccountBalance(account):
+
+
+def viewLog():
+
+
+def viewCharges():
+
+
+def chooseAccount():
+
